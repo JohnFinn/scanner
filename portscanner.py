9969d25bd730536074fb13ab249a9f6de54e115c
Jouny <dz4tune@gmail.com>
2018-09-04 21:31:22 +0300

    rewrote Scanner.scan method

--- portscanner.py
+++ portscanner.py
@@ -1,66 +1,51 @@
 #!/usr/bin/env python
-import socket, select, errno
-import asyncio, itertools
-from queue import Queue
-
-class Socket(socket.socket):
-    async def connect_async(self, address):
-        self.connect_ex(address)
-        while True:
-            await asyncio.sleep(0)
-            r,w,x = select.select([], [self], [self], 0) # TODO find out what x does
-            if w:
-                err = self.getsockopt(socket.SOL_SOCKET, socket.SO_ERROR)
-                if err == 0:
-                    return
-                elif err == errno.ECONNREFUSED:
-                    raise ConnectionRefusedError
-                else:
-                    raise ConnectionError(err)
-
+import socket
+from select import select
+from time import time
 
 class Scanner:
 
     def __init__(self, host):
         self.host = host
-        self._ports = Queue()
 
-    async def check_port(self, port:int):
-        s = Socket(socket.AF_INET, socket.SOCK_STREAM | socket.SOCK_NONBLOCK)
-        try:
-            await s.connect_async((self.host, port))
-        except ConnectionRefusedError:
-            return False
-        else:
-            return True
-        finally:
-            s.close()
+    # TODO make nonblocking by yielding at each iteration in while loop
+    def scan(self, ports, timeout=10):
+        sockets = {}
+        for port in ports:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM | socket.SOCK_NONBLOCK)
+            sockets[sock] = port
+            sock.connect_ex((self.host, port))
+        time_point = time() + timeout
+        while sockets and time() < time_point:
+            _, done, _ = select([],sockets.keys(),[],0)
+            for sock in done:
+                port = sockets[sock]
+                del sockets[sock]
+                yield port, sock.getsockopt(socket.SOL_SOCKET, socket.SO_ERROR) == 0
 
-    async def put_port(self, port:int):
-        self._ports.put((port, await self.check_port(port)))
-    # TODO yeild completed tasks before everyone else is complete
-    def scan(self, ports):
-        ports1, ports2 = itertools.tee(ports) # in order to know how many times we need to call get
-        loop = asyncio.get_event_loop()
-        tasks = asyncio.gather(*map(self.put_port, ports1))
-        loop.run_until_complete(tasks)
-        for port in ports2:
-            yield self._ports.get()
+    def open_ports(self, ports):
+        for port, is_open in self.scan(ports):
+            if is_open:
+                yield port
+        # return map(operator.itemgetter(0), filter(operator.itemgetter(1), ports))
 
-scanner = Scanner('scanme.nmap.org')
-for port, is_open in scanner.scan(range(20,25)):
-    if is_open:
-        print('[+]', port)
-    else:
-        print('[-]', port)
-
-for port in range(20,25):
-    s = socket.socket()
-    try:
-        s.connect(('scanme.nmap.org', port))
-    except ConnectionRefusedError:
-        print('[-]', port)
-    else:
-        print('[+]', port)
-    finally:
-        s.close()
+if __name__ == '__main__':
+    scanner = Scanner('scanme.nmap.org')
+    for port in scanner.open_ports(range(300)):
+        print(port)
+    # for port, is_open in scanner.scan(range(1024), 3):
+    #     if is_open:
+    #         print('[+]', port)
+    #     else:
+    #         print('[-]', port)
+# print('=============')
+# for port in range(30):
+#     s = socket.socket()
+#     try:
+#         s.connect(('scanme.nmap.org', port))
+#     except ConnectionRefusedError:
+#         print('[-]', port)
+#     else:
+#         print('[+]', port)
+#     finally:
+#         s.close()
