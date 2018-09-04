dff7bf2dd1b6758795a2deaf2f16e41cef04f2fb
Jouny <dz4tune@gmail.com>
2018-09-04 23:44:41 +0300

    maid scan a function and removed Scanner class

--- portscanner.py
+++ portscanner.py
@@ -4,36 +4,31 @@ from select import select
 from time import time
 import asyncio
 
-class Scanner:
 
-    def __init__(self, host):
-        self.host = host
+# TODO maybe there is a way to get port, a socket is trying to connect to and if there is, it won't be neccessary to store ports in dict
+async def scan(addresses):
+    sockets = {}
+    for address in addresses:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM | socket.SOCK_NONBLOCK)
+        sockets[sock] = address
+        sock.connect_ex(address)
+    while sockets:
+        await asyncio.sleep(0)
+        _, done, _ = select([],sockets.keys(),[],0)
+        for sock in done:
+            address = sockets[sock]
+            del sockets[sock]
+            yield address, sock.getsockopt(socket.SOL_SOCKET, socket.SO_ERROR) == 0
+    # I've found out that sockets will close implicitly, but don't know what about async functions
 
-    # TODO maybe there is a way to get port, a socket is trying to connect to and if there is, it won't be neccessary to store ports in dict
-    async def scan(self, ports):
-        sockets = {}
-        for port in ports:
-            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM | socket.SOCK_NONBLOCK)
-            sockets[sock] = port
-            sock.connect_ex((self.host, port))
-        while sockets:
-            await asyncio.sleep(0)
-            _, done, _ = select([],sockets.keys(),[],0)
-            for sock in done:
-                port = sockets[sock]
-                del sockets[sock]
-                yield port, sock.getsockopt(socket.SOL_SOCKET, socket.SO_ERROR) == 0
-        # I've found out that sockets will close implicitly, but don't know what about async functions
-
-    async def open_ports(self, ports):
-        async for port, is_open in self.scan(ports):
-            if is_open:
-                yield port
+async def open_ports(ports):
+    async for address, is_open in scan(ports):
+        if is_open:
+            yield address
 
 async def main():
-    scanner = Scanner('scanme.nmap.org')
-    async for port in scanner.open_ports(range(300)):
-        print(port)
+    async for address in open_ports(map(lambda port: ('scanme.nmap.org', port), range(300))):
+        print(address)
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
